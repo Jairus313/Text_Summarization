787c9a9af6704b4c8cbc7d41975d66b081fd6a2e
Sudeep Nellur <35619570+Jairus313@users.noreply.github.com>
2020-06-23 02:08:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+from summariser import Summariser
 
 app = Flask(__name__)
 
@@ -6,7 +7,15 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         text = request.form["text"]
-        return render_template("result.html",text=text)
+
+        paragraph = text
+        freq_table = Summariser.create_frequency_table(paragraph)
+        sentence = Summariser.sentence_tokenize(paragraph)
+        sentence_scores = Summariser.score_sentences(sentence, freq_table)
+        threshold = Summariser.find_average_score(sentence_scores)
+        summary = Summariser.generate_summary(sentence, sentence_scores, 0.75 * threshold)
+    
+        return render_template("result.html",text=summary)
     else:
         return render_template("home.html")
 
