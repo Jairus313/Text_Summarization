a417e2debefa03186bb06d03b269f7c17c7c2ca2
Sudeep Nellur <35619570+Jairus313@users.noreply.github.com>
2020-06-22 23:26:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -6,13 +6,10 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         text = request.form["text"]
-        return redirect(url_for("result"))
+        return render_template("result.html",text=text)
     else:
         return render_template("home.html")
 
-@app.route("/result")
-def result():
-    return render_template("result.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
