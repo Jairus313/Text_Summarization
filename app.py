7e0e6f52746e9d9b528226d3d441c46c965d634d
Sudeep Nellur <35619570+Jairus313@users.noreply.github.com>
2020-06-24 20:12:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,30 +3,27 @@ from summariser import Summariser
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
+    return render_template("home.html")
+
+
+@app.route("/predict", methods=["POST"])
+def result():
     if request.method == "POST":
         text = request.form["text"]
-
         if text == "":
             return render_template("home.html")
 
-        else:    
+        else:
             paragraph = Summariser.clean_text(text)
             freq_table = Summariser.create_frequency_table(paragraph)
             sentence = Summariser.sentence_tokenize(paragraph)
             sentence_scores = Summariser.score_sentences(sentence, freq_table)
             threshold = Summariser.find_average_score(sentence_scores)
             summary = Summariser.generate_summary(sentence, sentence_scores, 0.75 * threshold)
-        
+            
             return render_template("result.html",text=summary)
-    else:
-        return render_template("home.html")
-
-@app.route("/result")
-def result():
-    return render_template("result.html",text=summary)
-
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
